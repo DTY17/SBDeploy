6cc9a2fdadb2b516d55a4453344c502fd62d8ad0
DTY <dinanthemika.personal@gmail.com>
2025-08-04 10:15:10 +0530

    first

---
+++ src/test/java/lk/sbdeploy/SbDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sbdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
