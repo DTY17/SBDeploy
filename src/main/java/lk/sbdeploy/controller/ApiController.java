5d8e26400925b77afccd7d42d1b25223a4e0c174
DTY <dinanthemika.personal@gmail.com>
2025-08-04 10:28:36 +0530

    java 17

--- src/main/java/lk/sbdeploy/controller/ApiController.java
+++ src/main/java/lk/sbdeploy/controller/ApiController.java
@@ -1,6 +1,7 @@
 package lk.sbdeploy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,9 @@ public class ApiController {
     @GetMapping
     public String getData(){
         return "get Called";
+    }
+    @PutMapping("/put")
+    public String putData(){
+        return "put Called";
     }
 }
